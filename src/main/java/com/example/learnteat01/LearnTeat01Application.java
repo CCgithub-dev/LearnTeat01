c19511a07145b3b1e4e81d663ba81c566a68e0c2
CCgithub-dev <279053652@qq.com>
2024-05-13 22:22:46 +0800

    1

---
+++ src/main/java/com/example/learnteat01/LearnTeat01Application.java
@@ -0,0 +1,13 @@
+package com.example.learnteat01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTeat01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnTeat01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnteat01/LearnTeat01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnteat01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTeat01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
