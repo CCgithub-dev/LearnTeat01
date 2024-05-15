8259d465cf58e5011fa2c8c0f51612efa26d2383
CCgithub-dev <279053652@qq.com>
2024-05-15 19:03:15 +0800

    另一个模块的注入

--- src/main/java/com/example/learnteat01/LearnTeat01Application.java
+++ src/main/java/com/example/learnteat01/LearnTeat01Application.java
@@ -1,18 +1,30 @@
 package com.example.learnteat01;
 
+import com.example.learnconfig.config.CustDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+/**
+ * 用@ComponentScan,扫描包名可以获取
+ * 用@Import,注入类名,可以获取
+ *
+ *
+ */
+
 
 @SpringBootApplication
+//@ComponentScan("com.example.learnconfig.config")
+@Import(CustDemo.class)
 public class LearnTeat01Application {
 
     public static void main(String[] args) {
 
-        ConfigurableApplicationContext run = SpringApplication.run(LearnTeat01Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(LearnTeat01Application.class, args);
 
-        Object redisTemplate = run.getBean("user");
+        Object redisTemplate = context.getBean("cust");
         System.out.println(redisTemplate);
     }
 
